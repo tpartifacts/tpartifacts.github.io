bc9ead9900a8d8a95521dcaaf491dfb571ee86cd
abumusamq <mkhan3189@gmail.com>
2020-06-06 13:36:32 +1000

    remove

--- static/js/NotFound.64cd6245.chunk.js
+++
@@ -1,1 +0,0 @@
-(this["webpackJsonptaqwa-prayer"]=this["webpackJsonptaqwa-prayer"]||[]).push([[4],{137:function(t,n,a){},140:function(t,n,a){"use strict";a.r(n);var e=a(0),r=a.n(e);a(137);n.default=function(){return r.a.createElement("div",null,"404 Not Found!")}}}]);
